12ebf52c4a34f4f50edbb64899e00e0b9ae15f14
AaronCronnelly <101333389+AaronCronnelly@users.noreply.github.com>
2023-11-28 18:49:22 +0000

    Working on menu, having issue with not bening able to use javac and issue with not reconisgging color

--- OOPAssiment/src/ie/atu/sw/SearchAPI.java
+++ OOPAssiment/src/ie/atu/sw/SearchAPI.java
@@ -3,6 +3,27 @@ package ie.atu.sw;
 public class SearchAPI {
 	public void running() {
 		System.out.println("");
-		System.out.println("Conected to the api file");
+		System.out.println("Conected menu");
 	}
+
+	public void textFile() {
+		System.out.println("Specify text file");
+	}
+
+	public void url() {
+		System.out.println("Specify url");
+	}
+
+	public void outputFile() {
+		System.out.println("Specify output file");
+	}
+
+	public void confiLexicon() {
+		System.out.println("Confiure Lexicon");
+	}
+
+	public void exAnalyseReport() {
+		System.out.println("Execute analyse and report");
+	}
+
 }
